2eebcd5d3cdd72c07430750512c854aba5a69b54
admin <admin>
2026-09-14 12:07:20 +0700

    Initial Commit

---
+++ AnimeVerse/AnimeVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeVerseApp.swift
+//  AnimeVerse
+//
+//  Created by Revan Arturito on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimeVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
